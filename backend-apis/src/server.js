07c8248caefcf268c88f7ab1eaa7cb805cc071f1
ravikumarsadula <ravikumarsadula123@gmai.com>
2024-09-05 14:11:23 +0530

    done

---
+++ backend-apis/src/server.js
@@ -0,0 +1,19 @@
+const app = require('./app');
+const connectDB = require('./database');
+
+const PORT = process.env.PORT || 3000;
+const cors = require('cors')
+app.use(cors());
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    } catch (err) {
+        console.error('Server error:', err);
+        process.exit(1);
+    }
+};
+
+startServer();
